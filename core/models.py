d3630020efbb074e68212fb4f6a35126a61c5d96
Ankit Mittal <ankitmittal0111@gmail.com>
2015-05-18 23:10:25 +0530

    model finalize

--- core/models.py
+++ core/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    
+    def __unicode__(self):
+        return self.name 
+    
+        
 class Vendor(models.Model):
-    name = models.CharField(max_length=100)
-    key = models.CharField(max_length=100)
+    name = models.CharField(max_length=250)
+    contact_number = models.CharField(max_length=50)    
     address = models.TextField()
+    email = models.EmailField()
     users = models.ManyToManyField(User, through='UserMember', related_name='people')
     
     def __unicode__(self):
@@ -21,7 +28,15 @@ class UserMember(models.Model):
         return "%s belong to vendor %s (as %s)" % (self.user, self.vendor, self.role)
     
     
-class VendorDetail(models.Model):
-    type = models.CharField(max_length=50)
-    data_dict = models.TextField()
+class VendorField(models.Model):
     vendor = models.ForeignKey(Vendor, related_name='membership')
+    
+    
+class Fields(models.Model):    
+    name = models.CharField(max_length=250)
+    type = models.CharField(max_length=250)
+    length = models.IntegerField(max_length=50)
+    value = models.TextField()
+    
+    def __unicode__(self):
+        return self.name 
