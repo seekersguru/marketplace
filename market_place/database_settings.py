29b7fcc9c2a17dfe32595be973d20f29989fbffa
Ankit Mittal <ankit.write@gmail.com>
2015-05-25 23:53:34 +0530

    db credentials

--- market_place/database_settings.py
+++ market_place/database_settings.py
@@ -3,7 +3,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'marketplace',
         'USER': 'root',
-        'PASSWORD': 'ryoku2010',
+        'PASSWORD': 'yoku2010',
         'HOST': 'localhost',
         'PORT': '3306',
     }
