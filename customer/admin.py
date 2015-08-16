8521416e31846fc82486adf9e6b05ca07fee60a2
nishants <nisants@PRINHYLTPAP0219.local>
2015-08-16 10:23:39 +0530

    Customer Admin

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from models import Customer
+class CustomerAdmin(admin.ModelAdmin):
+    model = Customer 
+admin.site.register(Customer, CustomerAdmin)
 # Register your models here.
